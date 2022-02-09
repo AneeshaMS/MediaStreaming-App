f4c875621e1f6914bde21a34bafecf0fa7272418
AneeshaMS <88569462+AneeshaMS@users.noreply.github.com>
2022-02-09 13:58:51 +0530

    third commit

--- src/app/admin-panel/add-video/add-video.component.ts
+++ src/app/admin-panel/add-video/add-video.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
+
+
+export interface Tag {
+  name: string;
+}
+
 
 @Component({
   selector: 'app-add-video',
@@ -6,6 +14,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-video.component.css']
 })
 export class AddVideoComponent implements OnInit {
+
+
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  tags: Tag[] = [{name: 'video'}, {name: 'comedy'}, {name: 'add tag'}];
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.tags.push({name: value});
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(tag: Tag): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
 
   constructor() { }
 
--- src/app/admin-panel/edit-video/edit-video.component.ts
+++ src/app/admin-panel/edit-video/edit-video.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
+
+
+export interface Tag {
+  name: string;
+}
 
 @Component({
   selector: 'app-edit-video',
@@ -6,6 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-video.component.css']
 })
 export class EditVideoComponent implements OnInit {
+
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  tags: Tag[] = [{name: 'video'}, {name: 'comedy'}, {name: 'addtag'}];
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.tags.push({name: value});
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(tag: Tag): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
 
   constructor() { }
 
